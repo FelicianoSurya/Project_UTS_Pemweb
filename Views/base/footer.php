dc54b32829ff413f07e1fbecf2e30093b6c2f52b
Deiyak <dea.noveriyanti@gmail.com>
2021-10-07 20:29:41 +0700

    rapihin footer

--- Views/base/footer.php
+++ Views/base/footer.php
@@ -5,17 +5,16 @@
           <p>Bijak Melihat Dunia</p>
         </div>
         <div class="navfooter">
-          | <a href="index.php">Home</a> | <a href="AboutUs.php">About Us</a> |
+          <p>| <a href="index.php">HOME</a> | <a href="AboutUs.php">ABOUT US</a> |</p>
         </div>
       </div>
 
       <div class="katafooter text-center">
-        Ujian Tengah Semester - Pemograman Web <br />
+        <h6>Ujian Tengah Semester - Pemograman Web</h6>
         <?php foreach($kategories as $kategori){ ?> 
         <a onclick="redirectKategori(<?php echo $kategori->id ?>)"><?php echo $kategori->name ?> </a> |  <?php } ?> <br />
         <p>
-          @Newspaper | +62-9847458399 | 021-23647364 | Jl. Kebangsaan II, Gedung
-          News Paper
+          @Newspaper  |  +62-9847458399  |  021-23647364  |  Jl. Kebangsaan II, Gedung News Paper
         </p>
       </div>
 </footer>
